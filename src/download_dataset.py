fceae6e60b91472eca25e8ce33a1e75fb7579a2d
Gemma Isaac <138714701+ggi500@users.noreply.github.com>
2024-08-26 12:02:15 +0000

    download dataset update

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -1,12 +1,11 @@
 import kaggle
 import os
 
-
+# Define the directory to save the dataset
 data_dir = 'Data/UNSW-NB15'
 os.makedirs(data_dir, exist_ok=True)
 
-# Download the dataset
-
+# Download and unzip the dataset
 kaggle.api.dataset_download_files('mrwellsdavid/unsw-nb15', path=data_dir, unzip=True)
 
 print(f"Dataset downloaded and extracted to {data_dir}")
